6774336c038de33da1131c9a0c909de65d38f2fe
HgN <hgnguyen37@gmail.com>
2017-04-28 20:52:36 +0700

    code beautified

--- 1- Multiples of 3 and 5/main.c
+++ 1- Multiples of 3 and 5/main.c
@@ -1,24 +1,22 @@
-#include <stdio.h>
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
 
 uint32_t currentNumber = 0;
 uint32_t finalSum = 0;
 uint8_t table[] = {3, 2, 1, 3, 1, 2, 3};
 
-int main(void)
-{
-    uint8_t index = 0;
-    while(currentNumber<1000)
-    {
-        if(index>6)
-            index = 0;
-        currentNumber += table[index];
-        if(currentNumber<1000)
-            finalSum += currentNumber;
-        index++;
-    }
-    printf("finalSum = %i", finalSum);
-    printf("\n");
-    return 0;
+int main(void) {
+  uint8_t index = 0;
+  while (currentNumber < 1000) {
+    if (index > 6)
+      index = 0;
+    currentNumber += table[index];
+    if (currentNumber < 1000)
+      finalSum += currentNumber;
+    index++;
+  }
+  printf("finalSum = %i", finalSum);
+  printf("\n");
+  return 0;
 }
--- 2- Even Fibonacci numbers/main.c
+++ 2- Even Fibonacci numbers/main.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
-#include <stdint.h>
+#include <assert.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
 
 uint32_t finalSum = 0;
 uint32_t fibonacciNumTemp1 = 1;
@@ -8,22 +9,19 @@ uint32_t fibonacciNumTemp2 = 1;
 uint32_t fibonacciCurrentNum = 2;
 const uint32_t fibonacciMaxNum = 4000000;
 
-int main(void)
-{
-    while(fibonacciCurrentNum<fibonacciMaxNum)
-    {
-        printf("number = %i", fibonacciCurrentNum);
-        printf("\n");
-        finalSum += fibonacciCurrentNum;
-        int loop;
-        for(loop=0; loop<3; loop++)
-        {
-            fibonacciNumTemp1 = fibonacciNumTemp2;
-            fibonacciNumTemp2 = fibonacciCurrentNum;
-            fibonacciCurrentNum = fibonacciNumTemp1 + fibonacciNumTemp2;
-        } 
-    }
-    printf("finalSum = %i", finalSum);
+int main(void) {
+  while (fibonacciCurrentNum < fibonacciMaxNum) {
+    printf("number = %i", fibonacciCurrentNum);
     printf("\n");
-    return 0;
+    finalSum += fibonacciCurrentNum;
+    int loop;
+    for (loop = 0; loop < 3; loop++) {
+      fibonacciNumTemp1 = fibonacciNumTemp2;
+      fibonacciNumTemp2 = fibonacciCurrentNum;
+      fibonacciCurrentNum = fibonacciNumTemp1 + fibonacciNumTemp2;
+    }
+  }
+  printf("finalSum = %i", finalSum);
+  printf("\n");
+  return 0;
 }
