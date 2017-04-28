1a7bacf7acab8486dfad4fedfc97d3c7118145e4
HgN <hgnguyen37@gmail.com>
2017-04-28 20:31:38 +0700

    Problem 2 - Done

---
+++ 2- Even Fibonacci numbers/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+uint32_t finalSum = 0;
+uint32_t fibonacciNumTemp1 = 1;
+uint32_t fibonacciNumTemp2 = 1;
+uint32_t fibonacciCurrentNum = 2;
+const uint32_t fibonacciMaxNum = 4000000;
+
+int main(void)
+{
+    while(fibonacciCurrentNum<fibonacciMaxNum)
+    {
+        printf("number = %i", fibonacciCurrentNum);
+        printf("\n");
+        finalSum += fibonacciCurrentNum;
+        int loop;
+        for(loop=0; loop<3; loop++)
+        {
+            fibonacciNumTemp1 = fibonacciNumTemp2;
+            fibonacciNumTemp2 = fibonacciCurrentNum;
+            fibonacciCurrentNum = fibonacciNumTemp1 + fibonacciNumTemp2;
+        } 
+    }
+    printf("finalSum = %i", finalSum);
+    printf("\n");
+    return 0;
+}
