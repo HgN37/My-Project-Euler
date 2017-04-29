5e6bbf2a9ac3fa51b0f88728c6e37cd5c3251a03
HgN <hgnguyen37@gmail.com>
2017-04-29 21:06:00 +0700

    Problem 3 - Done

---
+++ 3- Largest prime factor/main.c
@@ -0,0 +1,28 @@
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+const uint64_t givenNumber = 600851475143;
+uint64_t factorNumber = 2;
+
+int main(void) {
+  bool flag = false;
+  uint8_t count = 0;
+  uint64_t tempNumber = givenNumber;
+  while (tempNumber != 1) {
+    while ((tempNumber % factorNumber) == 0) {
+      tempNumber = tempNumber / factorNumber;
+      flag = true;
+      count++;
+    }
+    if (flag == true) {
+      printf("%" PRIu64 "^%i\n", factorNumber, count);
+      flag = false;
+      count = 0;
+    }
+    factorNumber++;
+  }
+  printf("Largest prime factor: %" PRIu64 "\n", factorNumber - 1);
+  return 0;
+}
